243756e464b0d5c51fd905dcf95bfc8e49820866
PATRICK MALLERY <patrick@openfoodnetwork.org.uk>
2023-03-30 17:25:58 +0200

    added bulk of funcitonality and starting to sort the  styling

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-//pseudoCode
-
-
 
 //variables
 const boardcell = document.querySelector('.board-container');
@@ -8,29 +5,34 @@ const messageText = document.querySelector('.message')
 let currentPlayer = 1;
 let playerPlaces= ['','','','','','','','','',];
 const resetBtn = document.querySelector('.reset');
+const startBtn = document.querySelector('.start')
 const allBoardSpaces = document.querySelectorAll('.board-space');
+const mainSection = document.querySelector('main');
+let squareSelected = 0;
 
 
+// Start Button
+startBtn.addEventListener('click', function(){
+    allBoardSpaces.forEach(element => {
+        element.classList.remove('hidden')});
+    resetBtn.classList.remove('hidden');
+    mainSection.style.display = 'grid';
+    startBtn.classList.add('hidden')
+})
 
-// click function to add the symbol to the board
-boardcell.addEventListener('click', function(e) {
-    addSymbol(e);
-    });
-// function that adds player choice to the array
+
+// MAIN FUNCTION
 const playerChoice = function(num) {
- 
+    squareSelected = num;
     if(currentPlayer === 0 && playerPlaces[num] === '') {
         playerPlaces[num] = 'o';
         checkWinner('o');
-        checkTie()
-        // displayMessage(`Player 2 has picked square ${num +1} for their choice, It's player 1's turn now.`)
+        checkTie();
     } else if (currentPlayer === 1 && playerPlaces[num] === '') {
         playerPlaces[num] = 'x';
         checkWinner('x');
         checkTie();
-        // displayMessage(`Player 1 has picked square ${num +1} for their choice, It's player 2's turn now.`)
     }
-    console.log(playerPlaces);
 }
 
 //reset button
@@ -39,10 +41,11 @@ resetBtn.addEventListener('click', function() {
     console.log(playerPlaces);
     addSymbol('reset')
 })
-    
+
+
 // function that adds the correct symbol to the board and checks if the 
 //space is occupied based on currentPlayer variable
-const addSymbol = function(e) {
+boardcell.addEventListener('click',  function(e) {
     if (e === 'reset') {
         allBoardSpaces.forEach((element) => {
             element.classList.remove('x');
@@ -65,7 +68,7 @@ const addSymbol = function(e) {
         currentPlayer = 0;
         }
 
-}}
+}})
 
 //display message function
 
@@ -74,7 +77,7 @@ const displayMessage = function(message) {
 }
 
 
-//check winner or tie function
+//check winner function
 
 const checkWinner = function(symbol) {
     if ((playerPlaces[0] === symbol && playerPlaces[1] === symbol && playerPlaces[2] === symbol) || (playerPlaces[3] === symbol && playerPlaces[4] === symbol && playerPlaces[5] === symbol) || (playerPlaces[6] === symbol && playerPlaces[7] === symbol && playerPlaces[8] === symbol) || (playerPlaces[0] === symbol && playerPlaces[3] === symbol && playerPlaces[6] === symbol) || (playerPlaces[1] === symbol && playerPlaces[4] === symbol && playerPlaces[7] === symbol) || (playerPlaces[2] === symbol && playerPlaces[5] === symbol && playerPlaces[8] === symbol) || (playerPlaces[0] === symbol && playerPlaces[4] === symbol && playerPlaces[8] === symbol) || (playerPlaces[2] === symbol && playerPlaces[4] === symbol && playerPlaces[6] === symbol)){
@@ -83,11 +86,31 @@ const checkWinner = function(symbol) {
         } else {
             displayMessage('🎉🎉🎉Player 1 has won the game!!🎉🎉🎉');
         }
-    } 
+        allBoardSpaces.forEach(element => {
+            element.classList.add('hidden');
+            resetBtn.classList.add('hidden');
+            startBtn.classList.remove('hidden');
+            mainSection.style.display = 'block';
+        })
+    }  else {
+        if (currentPlayer === 0) {
+            displayMessage(`Player 2 has picked square ${squareSelected +1} for their choice, It's player 1's turn now.`);
+        } else {
+            displayMessage(`Player 1 has picked square ${squareSelected +1} for their choice, It's player 2's turn now.`);
+        }
+    }
 }
 
+
+// check tie function
 const checkTie = function() {
     if (playerPlaces.includes('') === false) {
         displayMessage('Players have tied');
+        allBoardSpaces.forEach(element => {
+            element.classList.add('hidden');
+            resetBtn.classList.add('hidden');
+            startBtn.classList.remove('hidden');
+            mainSection.style.display = 'block';
+        })
     }
 }
